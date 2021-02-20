463ccbd51229586c0f33229ac54a80ec2d4f194d
Cameron Wright <cam.mac.wright@gmail.com>
2021-02-19 20:16:28 -0800

    finished employee tracker

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const util = require("util");
+const { error } = require("console");
+const { DefaultDeserializer } = require("v8");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -13,7 +15,7 @@ const connection = mysql.createConnection({
 
 const query = util.promisify(connection.query).bind(connection);
 
-connection.connect(function(error) {
+connection.connect(function (error) {
     if (error) {
         throw error
     }
@@ -60,7 +62,7 @@ function menu() {
                     viewEmployees()
                     break;
                 // default: update employee roles.
-                default:
+                case "Update employee roles":
                     updateRoles()
                     break;
             }
@@ -78,91 +80,166 @@ function menu() {
 
 async function addDepartment() {
     const response = await inquirer
-        .prompt ([{
+        .prompt([{
             type: "input",
             message: "What department are you adding?",
             name: "department"
         }])
-        console.log(response)
-        const data = await query(`INSERT INTO department (name) VALUES ("${response.department}");`) 
-        menu()
+    console.log(response)
+    const data = await query(`INSERT INTO department (name) VALUES ("${response.department}");`)
+    menu()
 };
 
 async function addRoles() {
-    const response = await inquirer
-        .prompt ([
+
+    const depArr = await query("SELECT * FROM department")
+    const newDepArr = depArr.map(department => ({ name: department.name, value: department.id }))
+    const response = inquirer
+        .prompt([
             {
-            type: "input",
-            message: "What role are you adding?",
-            name: "role"
+                type: "input",
+                message: "What role are you adding?",
+                name: "role"
+            },
+            {
+                type: "input",
+                message: "What salary should this role have? Salary number MUST end in the hundredths place: e.g. 75000.00",
+                name: "salary"
+            },
+            {
+                type: "list",
+                message: "What department is this role in?",
+                choices: newDepArr,
+                name: "roleSal"
+
             }
         ])
-        console.log(response)
-        const data = await query(`INSERT INTO role (title) VALUES ("${response.role}");`)
-        menu()
+        .then(async response => {
+            console.log(response)
+            const data = await query("INSERT INTO role SET ?", {
+                title: response.role,
+                salary: response.salary,
+                department_id: response.roleSal
+            });
+            console.log("New role added.")
+            menu()
+        })
+
 };
 
-async function addEmployees() {
-    let roles = query(`SELECT DISTINCT title FROM role ORDER BY title;`);
-    console.log(roles)
-    let roleArr = Object.value(roles);
 
-    console.log(roleArr);
+
+
+
+async function addEmployees() {
+
+    // RowDataPacket is a normal constructor object in an array
+    // let roles = await query(`SELECT DISTINCT title FROM role ORDER BY id;`);
+    // console.log(roles)
+
+    // console.log(roles)
+    // let roleObj = (Object.values(roles));
+
+    // checking if array
+    // console.log([0].constructor.name);
+
+    //  may need to concat arrays? probably not though... one big array
+
+    // let roleArr = [i].;
+
+    // console.log(error, roleObj); 
+    // returns 8
+
+    // rolesArr[[role, id], [role, id]...]
+
+
+    // let rolesArr = [];
+    // roles.forEach((obj, i) => {
+    //     let roleAndId = [obj.title, roles[i]]
+    //     rolesArr.push(roleAndId)
+
+    //     // below works
+    //     // rolesArr.push(obj.title)
+    //     idNum = roles[i]
+    // });
+
+    //  psuedo column with id that passes along both name and number
+
+
+
+    // console.log((roles[0].title));
+    // console.log(rolesArr);
+    const rolesArr = await query("SELECT * FROM role")
+    const newRolesArr = rolesArr.map(role => ({ name: role.title, value: role.id }))
+
+    const employeeTable = await query("SELECT * FROM employee")
+    // console.log(employeeTable)
+    const newEmployeeTable = employeeTable.map(employee => ({ name: employee.first_name + " " + employee.last_name, value: employee.id }))
+    // console.log(newEmployeeTable)
     const response = await inquirer
-        .prompt ([
+        .prompt([
             {
-            type: "input",
-            message: "Employee's first name?",
-            name: "firstName"
+                type: "input",
+                message: "Employee's first name?",
+                name: "firstName"
             },
             {
-            type: "input",
-            message: "Employee's last name?",
-            name: "lastName"
+                type: "input",
+                message: "Employee's last name?",
+                name: "lastName"
             },
             {
-            type: "list",
-            message: "Employee's role?",
-            choices: [roleArr],
-            name: "empRole"
+                type: "list",
+                message: "Employee's role?",
+                choices: newRolesArr,
+                name: "empRole"
+            },
+            {
+                type: "list",
+                message: "Who is the manager?",
+                choices: newEmployeeTable,
+                name: "manager"
             }
         ])
 
-        // function roleFunc() {
-        //     let roleArr = Object.values(roles);
-        //     // for (let i= 0; i < roles.length; i++) {
-        //     //     roleArr.push(roles[i])
-        //     // }
+    const data = await query("INSERT INTO employee SET ?", {
+        first_name: response.firstName,
+        last_name: response.lastName,
+        role_id: response.empRole,
+        manager_id: response.manager
+    });
+    console.log("New employee added.")
+    menu()
+    // function roleFunc() {
+    //     let roleArr = Object.values(roles);
+    //     // for (let i= 0; i < roles.length; i++) {
+    //     //     roleArr.push(roles[i])
+    //     // }
 
 
 
 
-        //     return roleArr;
-        // };
+    //     return roleArr;
+    // };
 
-        // console.log(roleArr)
-        // for (let i = 0; i < data.length; i++) {
-        //     if (data[i].title === response.empRole) {
-        //         response.role_id = data[i].id;
-        //     }
-        // } 
+    // console.log(roleArr)
+    // for (let i = 0; i < data.length; i++) {
+    //     if (data[i].title === response.empRole) {
+    //         response.role_id = data[i].id;
+    //     }
+    // } 
 
-        console.log(response)
-        const data = await query("INSERT INTO employee SET ?", {
-            first_name: response.firstName,
-            last_name: response.lastName,
-            role_id: response.role_id
-        }, (err, data) => {
-            if (err) throw err;
-        });
-        console.table(data)
-        menu()
-    };
+
+
+
+
+   
+};
 
 async function viewDepartments() {
-   const data = await query("SELECT * FROM department;")
-   console.table(data)
-   menu()
+    const data = await query("SELECT * FROM department;")
+    console.table(data)
+    menu()
 };
 
 async function viewRoles() {
@@ -172,11 +249,63 @@ async function viewRoles() {
 };
 
 async function viewEmployees() {
-    const data = await query("SELECT employee.id, employee.first_name, employee.last_name, role.title, FROM employee, role WHERE (employee.role_id=role.id) OR role_id IS NULL;")
+    // todo left join
+    // left join shows all rows in table
+    // this will show when there is a primary key and secondary key match
+    const data = await query("SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee left join role ON employee.role_id=role.id;")
     console.table(data)
     menu()
 };
 
-function updateRoles() {
+// let the user pick the employee to update the employee role
 
+
+async function updateRoles() {
+    // viewing employees
+    const employeeTable = await query("SELECT * FROM employee")
+    console.log(employeeTable)
+    const newEmployeeTable = employeeTable.map(employee => ({ name: employee.first_name + " " + employee.last_name, value: employee.id }))
+    console.log(newEmployeeTable)
+
+
+
+    // viewing roles to map
+    const rolesArr = await query("SELECT * FROM role")
+    const newRolesArr = rolesArr.map(role => ({ name: role.title, value: role.id }))
+
+    const response = await inquirer
+        .prompt([
+            // for update employees role, not implemented.
+            // {
+            //     type: "input",
+            //     message: "Empolyee's first name for update:",
+            //     name: "updateFirst"
+            // },
+            // {
+            //     type: "input",
+            //     message: "Employee's last name for update:",
+            //     name: "employeeLast"
+            // },
+
+            {
+                type: "list",
+                message: "Update info on which employee?",
+                choices: newEmployeeTable,
+                name: "employeeId"
+            },
+            {
+                type: "list",
+                message: "Update which role?",
+                choices: newRolesArr,
+                name: "roleId"
+            } 
+        ])
+    const data = await query("UPDATE employee SET ? WHERE ?",
+        [
+            {role_id: response.roleId },
+            { id: response.employeeId}
+        ])
+        console.log("Employees new role updated.")
+        menu()
+    
 };
